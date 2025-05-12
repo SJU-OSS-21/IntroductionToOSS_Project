ea825a97b8372a92b8eab7aa1ee9042fcc7959d8
GGGGGangSub <fprhrkdwns11511@gmail.com>
2025-05-12 18:53:44 +0900

    HOTFIX : Player class Fix

--- Project Directory/src/Player_Item/Model/Player.java
+++ Project Directory/src/Player_Item/Model/Player.java
@@ -1,6 +1,8 @@
-package Player_Item/Model;
+package Player_Item.Model;
 
+import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
 public class Player {
     private int x, y; // 플레이어 위치
@@ -18,14 +20,15 @@ public class Player {
 
     // 이동 함수
     public void move(int dx, int dy, int maxW, int maxH) {
-
+        x = Math.max(0, Math.min(x + dx * speed, maxW - image.getWidth(null)));
+        y = Math.max(0, Math.min(y + dy * speed, maxH - image.getHeight(null)));
     }
     // draw()
     public void draw(Graphics g) {
-
+        g.drawImage(image, x, y, null);
     }
     // 경계값
     public Rectangle getBounds() {
-
+        return new Rectangle(x, y, image.getWidth(null), image.getHeight(null));
     }
 }
