26a7bc2c27f9656b65332e2b4dfe5c407dfe1b87
yanggogi1423 <yanggogi1423@gmail.com>
2025-06-07 03:35:22 +0900

    Feat : UI Panel Base

---
+++ Project Directory/src/UI_Scene/InGameUIPanel.java
@@ -0,0 +1,24 @@
+package UI_Scene;
+
+import java.awt.*;
+
+public class InGameUIPanel extends UIPanel {
+
+    private int score = 0;
+
+    public InGameUIPanel(int width, int height) {
+        super(width, height);
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+        repaint();
+    }
+
+    @Override
+    protected void drawUI(Graphics2D g2) {
+        g2.setFont(customFont.deriveFont(18f));
+        g2.setColor(Color.YELLOW);
+        g2.drawString("Score: " + score, 20, 30);
+    }
+}
