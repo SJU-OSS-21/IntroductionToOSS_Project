c29e482f70f187a9023f13790968ffc64a8c02b5
hjd5031 <hjd01034215031@gmail.com>
2025-06-08 01:20:46 +0900

    File: Add bgm resource

--- Project Directory/src/Map_Audio/SoundManager.java
+++ Project Directory/src/Map_Audio/SoundManager.java
@@ -1,4 +1,19 @@
 package Map_Audio;
 
+import javax.sound.sampled.*;
+
 public class SoundManager {
+    private static final int SONG_COUNT = 5;//재생할 노래의 수
+    private static Clip[] _clips = new Clip[SONG_COUNT];
+    static{
+        try{
+            for(int i = 0; i < SONG_COUNT; i++){
+                AudioInputStream ais = AudioSystem.getAudioInputStream(SoundManager.class.getResource("song"+i+".wav"));
+                _clips[i] = AudioSystem.getClip();
+                _clips[i].open(ais);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
